d671940158f923f6b20e741d288577ad8c408ef1
fungkinchun <fungkinchun@gmail.com>
2026-03-08 16:12:09 +0000

    first commit

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -372,7 +372,6 @@ resource "aws_iam_policy" "alb_controller_policy" {
         ],
         "Condition" : {
           "Null" : {
-            "aws:RequestTag/elbv2.k8s.aws/cluster" : "true",
             "aws:ResourceTag/elbv2.k8s.aws/cluster" : "false"
           }
         }
