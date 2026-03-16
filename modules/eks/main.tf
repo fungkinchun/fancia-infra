493782fcf04f121da11fcb290e379ec534f0ab8e
fungkinchun <fungkinchun@gmail.com>
2026-03-16 17:01:26 +0000

    first commit

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -129,13 +129,6 @@ resource "aws_iam_role" "pod_role" {
             ]
           }
         }
-      },
-      {
-        Effect    = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.namespace}-pod-role"
-        }
-        Action    = "sts:AssumeRole"
       }
     ]
   })
